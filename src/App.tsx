a1e10205fb79f2171067b0573e122ccd128c2dd0
Ezequias Torczuk <144610780+MrDadoxx@users.noreply.github.com>
2024-08-04 20:27:36 -0300

    App.tsx removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
 import "./App.css";
-import { Main } from "./components/Main";
-import { GameInfo } from "./components/GameInfo";
-import { Routes, Route } from "react-router-dom";
-import { Privacy } from "./components/Privacy";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/krypto" element={<GameInfo name="krypto" />} />
-      <Route path="/krypto/privacy" element={<Privacy name="krypto" />}></Route>
-      <Route path="/thenorisproject" element={<GameInfo name="tnp" />} />
-    </Routes>
-  );
+  return;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
+import { GameInfo } from "./components/GameInfo.tsx";
+import { Privacy } from "./components/Privacy.tsx";
+import { Main } from "./components/Main.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/krypto" element={<GameInfo name="krypto" />} />
+      <Route path="/krypto/privacy" element={<Privacy name="krypto" />}></Route>
+      <Route path="/thenorisproject" element={<GameInfo name="tnp" />} />
+    </Routes>
+  </Router>
 );
