72962c5aca5a0344d365bc3e2c6c343bfad335ed
Ezequias Torczuk <144610780+MrDadoxx@users.noreply.github.com>
2024-08-12 14:31:55 -0300

    Responsive bug fix #2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,15 @@ import { Loading } from "./components/Loading";
 const LazyGameInfo = React.lazy(() => import("./components/GameInfo"));
 
 function App() {
+  const setVh = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  };
+
+  window.addEventListener("resize", setVh);
+  window.addEventListener("orientationchange", setVh);
+  setVh();
+
   return (
     <Suspense fallback={<Loading />}>
       <Routes>
