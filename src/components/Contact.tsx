62555cb4cdf16a96c4c29603332649439f24892b
Ezequias Torczuk <144610780+MrDadoxx@users.noreply.github.com>
2024-08-19 15:04:42 -0300

    Contact input-name changed

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import { images } from "../constants";
 import "./Contact.css";
 
 export function Contact() {
+  const [name, setName] = useState<string>("");
   const mail: string = "alexe1developer777@gmail.com";
   const formAction: string = `https://formsubmit.co/${mail}`;
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (/^[^\d]*$/.test(value)) {
+      setName(value);
+    }
+  };
 
   return (
     <section id="contact">
       <form className="contact__form" action={formAction} method="POST">
         <div className="contact__form__name">
           <img src={images.name} alt="Name" />
-          <input type="text" placeholder="Name" name="name" required />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
         </div>
         <div className="contact__form__email">
           <img src={images.email} alt="Email" />
